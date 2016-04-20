a3c25bf3c62f94b251064bdbdf194e7ed3075454
Lily Cartelet <flowercartelet@gmail.com>
2016-04-20 10:01:53 +0900

    Merge pull request eorzea-weather/node-eorzea-time#4 from flowercartelet/ava
    Add a tests using the AVA

---
+++ packages/eorzea-time/test/eorzea-time-with-argument.js
@@ -0,0 +1,30 @@
+import test from 'ava';
+import EorzeaTime from '../lib/eorzea-time';
+
+test(function toString(t) {
+  const unixTime = Date.UTC(2015, 6, 23, 9, 0, 0);
+  const date = new Date(unixTime);
+  const eorzeaTime = new EorzeaTime(date);
+  t.deepEqual(eorzeaTime.toString(), '17:05:34');
+});
+
+test(function getHours(t) {
+  const unixTime = Date.UTC(2015, 6, 23, 9, 0, 0);
+  const date = new Date(unixTime);
+  const eorzeaTime = new EorzeaTime(date);
+  t.deepEqual(eorzeaTime.getHours(), 17);
+});
+
+test(function getMinutes(t) {
+  const unixTime = Date.UTC(2015, 6, 23, 9, 0, 0);
+  const date = new Date(unixTime);
+  const eorzeaTime = new EorzeaTime(date);
+  t.deepEqual(eorzeaTime.getMinutes(), 5);
+});
+
+test(function getSeconds(t) {
+  const unixTime = Date.UTC(2015, 6, 23, 9, 0, 0);
+  const date = new Date(unixTime);
+  const eorzeaTime = new EorzeaTime(date);
+  t.deepEqual(eorzeaTime.getSeconds(), 34);
+});
