fbb5c64890ed2661f4148d878bc88b37a3169942
Lily Cartelet <flowercartelet@gmail.com>
2018-03-24 13:06:00 +0900

    Change background on currently date

--- src/pages/Zone.jsx
+++ src/pages/Zone.jsx
@@ -1,11 +1,13 @@
+import classNames from 'classnames';
 import Paper from 'material-ui/Paper';
+import { withStyles } from 'material-ui/styles';
 import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
 import Typography from 'material-ui/Typography';
 import camelCase from 'lodash/camelCase';
 import range from 'lodash/range';
 import PropTypes from 'prop-types';
 import React, { Fragment, PureComponent } from 'react';
-import { FormattedMessage, FormattedTime, intlShape } from 'react-intl';
+import { FormattedMessage, FormattedTime, injectIntl, intlShape } from 'react-intl';
 import EorzeaWeather from '../eorzea-weather';
 import zones from '../zones.json';
 
@@ -17,21 +19,26 @@ const getStartTime = (date) => {
   return new Date(startUnixtime * 1000);
 };
 
-export default class Zone extends PureComponent {
-  static contextTypes = {
-    intl: intlShape.isRequired,
-  };
+export const styles = {
+  activeTableCell: {
+    backgroundColor: 'rgba(0, 0, 0, .05)',
+  },
+};
 
+@injectIntl
+@withStyles(styles)
+export default class Zone extends PureComponent {
   static propTypes = {
-    // eslint-disable-next-line react/forbid-prop-types
-    match: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+    intl: intlShape.isRequired,
+    match: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
   };
 
   getCurrentZoneName() {
-    const { intl } = this.context;
+    const { locale } = this.props.intl;
     const zoneId = this.getCurrentZoneId();
     const zone = zones[zoneId];
-    return zone[intl.locale] || zone.en;
+    return zone[locale] || zone.en;
   }
 
   getCurrentZoneId() {
@@ -40,13 +47,15 @@ export default class Zone extends PureComponent {
   }
 
   getWeather(msec) {
-    const { locale } = this.context.intl;
+    const { locale } = this.props.intl;
     const zoneId = this.getCurrentZoneId();
     return EorzeaWeather.getWeather(msec, { zoneId, locale });
   }
 
   render() {
+    const { classes } = this.props;
     const zoneName = this.getCurrentZoneName();
+    const now = Date.now();
     const startTime = getStartTime(new Date()).getTime();
     const step = 8 * 175 * 1000; // 8 hours
 
@@ -68,8 +77,11 @@ export default class Zone extends PureComponent {
                   <TableRow key={`row-${dayStartTime}`}>
                     {range(dayStartTime, dayStartTime + (step * 3), step).map((time) => {
                       const weather = this.getWeather(time);
+                      const className = classNames({
+                        [classes.activeTableCell]: time <= now && now < time + step,
+                      });
                       return (
-                        <TableCell key={`cell-${time}`}>{weather} (<FormattedTime value={new Date(time)} />)</TableCell>
+                        <TableCell className={className} key={`cell-${time}`}>{weather} (<FormattedTime value={new Date(time)} />)</TableCell>
                       );
                     })}
                   </TableRow>
