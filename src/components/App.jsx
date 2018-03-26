ae9aee7915a80cbb9e0cbf8f7fe16932ed92c57b
Lily Cartelet <flowercartelet@gmail.com>
2018-03-26 17:18:01 +0900

    Add Eorzea clock

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,7 @@ import { Link, Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import NoMatch from '../pages/NoMatch';
 import Zone from '../pages/Zone';
+import EorzeaClock from './EorzeaClock';
 
 export const styles = {
   appbarTitle: {
@@ -76,6 +77,7 @@ export default class App extends Component {
             <Typography className={classes.flex} color="inherit" noWrap variant="title">
               <Link className={classes.appbarTitle} to="/">Eorzea Weather</Link>
             </Typography>
+            <EorzeaClock />
             <IconButton color="inherit" onClick={this.handleLanguageIconClick}>
               <LanguageIcon />
             </IconButton>
