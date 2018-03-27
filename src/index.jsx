d81846fc431fba86b159c4ce08e0e7fe0ea937ff
Lily Cartelet <flowercartelet@gmail.com>
2018-03-27 13:06:31 +0900

    Refactor

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,8 @@
 import localForage from 'localforage';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { addLocaleData, IntlProvider } from 'react-intl';
-import jaLocaleData from 'react-intl/locale-data/ja';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './components/App';
-import enMessages from './locales/en.json';
-import jaMessages from './locales/ja.json';
-
-addLocaleData(jaLocaleData);
+import Main from './components/Main';
 
 const getCurrentLocale = async () => {
   const cachedLocale = await localForage.getItem('locale');
@@ -36,13 +30,10 @@ const render = (element, container) => new Promise((resolve, reject) => {
 const main = async () => {
   const container = document.getElementById('root');
   const locale = await getCurrentLocale();
-  const messages = locale === 'ja' ? jaMessages : enMessages;
   const element = (
-    <IntlProvider locale={locale} messages={messages}>
-      <Router>
-        <App />
-      </Router>
-    </IntlProvider>
+    <Router>
+      <Main locale={locale} />
+    </Router>
   );
   await render(element, container);
 };
