8b8c93a1e9783f69cedeb2389998e98718f98a5f
Lily Cartelet <flowercartelet@gmail.com>
2018-04-05 16:07:19 +0900

    Refactor (eorzea-weather/node-eorzea-weather#6)

--- packages/eorzea-weather/src/EorzeaWeather.js
+++ packages/eorzea-weather/src/EorzeaWeather.js
@@ -1,34 +1,171 @@
 import * as weatherChances from './chances';
 import * as locales from './locales/index';
+import calculateForecastTarget from './utils/calculateForecastTarget';
 import * as zones from './zones';
 
 const DEFAULT_LOCALE = 'en';
 
-// Thanks to Rogueadyn's SaintCoinach library for this calculation.
-// lDate is the current local time.
-const calculateForecastTarget = (date) => {
-  const unixtime = Math.floor(date.getTime() / 1000);
-  // Get Eorzea hour for weather start
-  const bell = unixtime / 175;
-
-  // Do the magic 'cause for calculations 16:00 is 0, 00:00 is 8 and 08:00 is 16
-  const increment = ((bell + 8) - (bell % 8)) % 24;
-
-  // Take Eorzea days since unix epoch
-  let totalDays = unixtime / 4200;
-  totalDays = (totalDays << 32) >>> 0; // eslint-disable-line no-bitwise
-
-  const calcBase = (totalDays * 0x64) + increment;
-
-  /* eslint-disable no-bitwise */
-  const step1 = ((calcBase << 0xB) ^ calcBase) >>> 0;
-  const step2 = ((step1 >>> 8) ^ step1) >>> 0;
-  /* eslint-enable no-bitwise */
-
-  return step2 % 0x64;
-};
-
 export default class EorzeaWeather {
+  static get ZONE_AZYS_LLA() {
+    return zones.ZONE_AZYS_LLA;
+  }
+
+  static get ZONE_CENTRAL_SHROUD() {
+    return zones.ZONE_CENTRAL_SHROUD;
+  }
+
+  static get ZONE_CENTRAL_THANALAN() {
+    return zones.ZONE_CENTRAL_THANALAN;
+  }
+
+  static get ZONE_COERTHAS_CENTRAL_HIGHLANDS() {
+    return zones.ZONE_COERTHAS_CENTRAL_HIGHLANDS;
+  }
+
+  static get ZONE_COERTHAS_WESTERN_HIGHLANDS() {
+    return zones.ZONE_COERTHAS_WESTERN_HIGHLANDS;
+  }
+
+  static get ZONE_EASTERN_LA_NOSCEA() {
+    return zones.ZONE_EASTERN_LA_NOSCEA;
+  }
+
+  static get ZONE_EASTERN_THANALAN() {
+    return zones.ZONE_EASTERN_THANALAN;
+  }
+
+  static get ZONE_EAST_SHROUD() {
+    return zones.ZONE_EAST_SHROUD;
+  }
+
+  static get ZONE_EUREKA_ANEMOS() {
+    return zones.ZONE_EUREKA_ANEMOS;
+  }
+
+  static get ZONE_GRIDANIA() {
+    return zones.ZONE_GRIDANIA;
+  }
+
+  static get ZONE_IDYLLSHIRE() {
+    return zones.ZONE_IDYLLSHIRE;
+  }
+
+  static get ZONE_ISHGARD() {
+    return zones.ZONE_ISHGARD;
+  }
+
+  static get ZONE_KUGANE() {
+    return zones.ZONE_KUGANE;
+  }
+
+  static get ZONE_LIMSA_LOMINSA() {
+    return zones.ZONE_LIMSA_LOMINSA;
+  }
+
+  static get ZONE_LOWER_LA_NOSCEA() {
+    return zones.ZONE_LOWER_LA_NOSCEA;
+  }
+
+  static get ZONE_MIDDLE_LA_NOSCEA() {
+    return zones.ZONE_MIDDLE_LA_NOSCEA;
+  }
+
+  static get ZONE_MIST() {
+    return zones.ZONE_MIST;
+  }
+
+  static get ZONE_MOR_DHONA() {
+    return zones.ZONE_MOR_DHONA;
+  }
+
+  static get ZONE_NORTHERN_THANALAN() {
+    return zones.ZONE_NORTHERN_THANALAN;
+  }
+
+  static get ZONE_NORTH_SHROUD() {
+    return zones.ZONE_NORTH_SHROUD;
+  }
+
+  static get ZONE_OUTER_LA_NOSCEA() {
+    return zones.ZONE_OUTER_LA_NOSCEA;
+  }
+
+  static get ZONE_RHALGRS_REACH() {
+    return zones.ZONE_RHALGRS_REACH;
+  }
+
+  static get ZONE_SOUTHERN_THANALAN() {
+    return zones.ZONE_SOUTHERN_THANALAN;
+  }
+
+  static get ZONE_SOUTH_SHROUD() {
+    return zones.ZONE_SOUTH_SHROUD;
+  }
+
+  static get ZONE_THE_AZIM_STEPPE() {
+    return zones.ZONE_THE_AZIM_STEPPE;
+  }
+
+  static get ZONE_THE_CHURNING_MISTS() {
+    return zones.ZONE_THE_CHURNING_MISTS;
+  }
+
+  static get ZONE_THE_DRAVANIAN_FORELANDS() {
+    return zones.ZONE_THE_DRAVANIAN_FORELANDS;
+  }
+
+  static get ZONE_THE_DRAVANIAN_HINTERLANDS() {
+    return zones.ZONE_THE_DRAVANIAN_HINTERLANDS;
+  }
+
+  static get ZONE_THE_FRINGES() {
+    return zones.ZONE_THE_FRINGES;
+  }
+
+  static get ZONE_THE_GOBLET() {
+    return zones.ZONE_THE_GOBLET;
+  }
+
+  static get ZONE_THE_LAVENDER_BEDS() {
+    return zones.ZONE_THE_LAVENDER_BEDS;
+  }
+
+  static get ZONE_THE_LOCHS() {
+    return zones.ZONE_THE_LOCHS;
+  }
+
+  static get ZONE_THE_PEAKS() {
+    return zones.ZONE_THE_PEAKS;
+  }
+
+  static get ZONE_THE_RUBY_SEA() {
+    return zones.ZONE_THE_RUBY_SEA;
+  }
+
+  static get ZONE_THE_SEA_OF_CLOUDS() {
+    return zones.ZONE_THE_SEA_OF_CLOUDS;
+  }
+
+  static get ZONE_ULDAH() {
+    return zones.ZONE_ULDAH;
+  }
+
+  static get ZONE_UPPER_LA_NOSCEA() {
+    return zones.ZONE_UPPER_LA_NOSCEA;
+  }
+
+  static get ZONE_WESTERN_LA_NOSCEA() {
+    return zones.ZONE_WESTERN_LA_NOSCEA;
+  }
+
+  static get ZONE_WESTERN_THANALAN() {
+    return zones.ZONE_WESTERN_THANALAN;
+  }
+
+  static get ZONE_YANXIA() {
+    return zones.ZONE_YANXIA;
+  }
+
   static getWeather(zoneId, date, options = {}) {
     return new EorzeaWeather(zoneId, options).getWeather(date);
   }
@@ -59,10 +196,7 @@ export default class EorzeaWeather {
   }
 
   validate() {
-    return Object.keys(zones).some(key => zones[key] === this.zoneId);
+    const key = `ZONE_${this.zoneId.replace(/[A-Z]/g, w => `_${w}`).toUpperCase()}`;
+    return EorzeaWeather[key] === this.zoneId;
   }
 }
-
-Object.keys(zones).forEach((key) => {
-  EorzeaWeather[key] = zones[key];
-});
