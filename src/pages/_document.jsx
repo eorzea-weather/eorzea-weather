f030adcf26cae5a18d72c473fe2de3813ca47b0c
Lily Cartelet <flowercartelet@gmail.com>
2020-07-14 05:08:52 +0900

    Fix invalid syntax

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -63,12 +63,12 @@ class MyDocument extends Document {
           {process.env.NEXT_PUBLIC_GA_TRACKING_ID && (
             <>
               <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`} />
-              {/* eslint-disable-next-line react/no-danger */}
               <script
+                /* eslint-disable-next-line react/no-danger */
                 dangerouslySetInnerHTML={{
-                  __html: createTrackingCode(process.env.NEXT_PUBLIC_GA_TRACKING_ID)
+                  __html: createTrackingCode(process.env.NEXT_PUBLIC_GA_TRACKING_ID),
                 }}
-               />
+              />
             </>
           )}
           {helmet.title.toComponent()}
