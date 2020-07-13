c5a9e10affedc6ce046fa922b6552fcc3ece4fa7
Lily Cartelet <flowercartelet@gmail.com>
2020-07-14 05:11:49 +0900

    Add tracking id to config (#389)
    * Add tracking id to config
    
    * Fix env name
    
    * Fix sysntax
    
    * Fix invalid syntax

--- src/utils/tracker.js
+++ src/utils/tracker.js
@@ -1,7 +1,7 @@
 export default {
   track: ({ path, title }) => {
     if (typeof window.gtag === 'function') {
-      window.gtag('config', process.env.GOOGLE_ANALYTICS_TRACKING_ID, {
+      window.gtag('config', process.env.NEXT_PUBLIC_GA_TRACKING_ID, {
         page_path: path,
         page_title: title,
       });
