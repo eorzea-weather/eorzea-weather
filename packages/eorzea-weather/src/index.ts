c84a94dbf548fa5a413662e052e4a7abaefe6827
Lily Cartelet <flowercartelet@gmail.com>
2020-07-20 07:54:20 +0900

    Introduce Prettier (eorzea-weather/node-eorzea-weather#215)

--- packages/eorzea-weather/src/index.ts
+++ packages/eorzea-weather/src/index.ts
@@ -1,1 +1,1 @@
-export { default } from './EorzeaWeather'
+export { default } from './EorzeaWeather';
--- packages/eorzea-weather/src/utils/calculateForecastTarget.ts
+++ packages/eorzea-weather/src/utils/calculateForecastTarget.ts
@@ -6,16 +6,16 @@ export default (date: Date): number => {
   const bell = unixtime / 175;
 
   // Do the magic 'cause for calculations 16:00 is 0, 00:00 is 8 and 08:00 is 16
-  const increment = ((bell + 8) - (bell % 8)) % 24;
+  const increment = (bell + 8 - (bell % 8)) % 24;
 
   // Take Eorzea days since unix epoch
   let totalDays = unixtime / 4200;
   totalDays = (totalDays << 32) >>> 0; // eslint-disable-line no-bitwise
 
-  const calcBase = (totalDays * 0x64) + increment;
+  const calcBase = totalDays * 0x64 + increment;
 
   /* eslint-disable no-bitwise */
-  const step1 = ((calcBase << 0xB) ^ calcBase) >>> 0;
+  const step1 = ((calcBase << 0xb) ^ calcBase) >>> 0;
   const step2 = ((step1 >>> 8) ^ step1) >>> 0;
   /* eslint-enable no-bitwise */
 
