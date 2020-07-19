07570c69516f86a7b86deca697571ef79044cb5c
Lily Cartelet <flowercartelet@gmail.com>
2020-07-20 00:13:29 +0900

    Add props to redirector

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,26 @@
 import accepts from 'accepts';
 import { AVAILABLE_LOCALES } from '../constants';
 
+const availableLocales = Object.keys(AVAILABLE_LOCALES)
+
 export const getServerSideProps = async ({ query, res, req }) => {
   const accept = accepts(req);
-  const locales = Object.keys(AVAILABLE_LOCALES);
-  const locale = locales.includes(query.locale)
+  const locale = availableLocales.includes(query.locale)
     ? query.locale
-    : accept.language(locales) || 'en';
-  const path = req.url === '/' ? '' : req.url;
+    : accept.language(availableLocales) || 'en';
+  const path = req.url.split('?')[0];
+  const newPath = `/${locale}${path === '/' ? '' : path}`;
 
-  res.writeHead(307, {
-    Location: `/${locale}${path}`,
-  });
-  res.end();
+  if (res) {
+    res.writeHead(307, {
+      Location: newPath,
+    });
+    res.end();
+  }
+
+  return {
+    props: {},
+  };
 };
 
 const Redirect = () => null;
