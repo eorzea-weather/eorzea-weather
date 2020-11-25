2d677317bae52d8aa1cd960df481b5847a2daf51
Lily Cartelet <flowercartelet@gmail.com>
2020-11-26 00:12:04 +0900

    Use useCallback (#589)
    * Use useCallback
    
    * Fix syntax

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,8 @@ import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import { I18nProvider } from '@react-aria/i18n';
 import type { AppProps } from 'next/app';
 import Router from 'next/router';
-import React, { useEffect } from 'react';
 import type { FC } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import Layout from '@/components/Layout';
 import { Provider as ZoneProvider } from '@/context/zone';
 import tracker from '@/utils/tracker';
@@ -18,20 +18,20 @@ const theme = createMuiTheme({
 const MyApp: FC<AppProps> = ({ Component, pageProps, router }) => {
   const locale = router.locale ?? 'en';
 
-  useEffect(() => {
-    const handleRouteChangeComplete = (url: string) => {
-      tracker.track({
-        path: url,
-        title: document.title,
-      });
-    };
+  const handleRouteChangeComplete = useCallback((url: string) => {
+    tracker.track({
+      path: url,
+      title: document.title,
+    });
+  }, []);
 
+  useEffect(() => {
     Router.events.on('routeChangeComplete', handleRouteChangeComplete);
 
     return () => {
       Router.events.off('routeChangeComplete', handleRouteChangeComplete);
     };
-  }, []);
+  }, [handleRouteChangeComplete]);
 
   useEffect(() => {
     const renderedStyles = document.getElementById('jss-server-side');
