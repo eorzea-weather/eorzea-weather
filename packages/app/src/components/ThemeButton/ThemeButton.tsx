8126b2816278dddcd32f2a5a13c1ae292b73f9a7
Khloe Leclair <xivkhloeleclair@gmail.com>
2021-06-01 14:24:47 -0400

    Add a localized tool-tip for the theme select button. Translation via DeepL and Google Translate, as I do not speak Japanese.

--- packages/app/src/components/ThemeButton/ThemeButton.tsx
+++ packages/app/src/components/ThemeButton/ThemeButton.tsx
@@ -1,26 +1,25 @@
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
 import MoonIcon from '@material-ui/icons/Brightness4';
 import SunIcon from '@material-ui/icons/BrightnessHigh';
-import React, { FC, useContext, useCallback } from 'react';
-import { SettingsContext } from '../context/settings';
+import { useMessageFormatter } from '@react-aria/i18n';
+import React, { FC, useCallback } from 'react';
+import { useSettings } from '../../context/settings';
+import messages from './intl';
 
 const ThemeButton: FC = () => {
-  const settings = useContext(SettingsContext);
+  const messageFormatter = useMessageFormatter(messages);
+  const settings = useSettings();
   const prefersDark = useMediaQuery('(prefers-color-scheme: dark)');
 
-  // This isn't unsafe. TypeScript go away.
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
   let dark = settings.state.dark;
   if (dark == null) dark = prefersDark;
 
   const handleThemeIconClick = useCallback(() => {
-    // This isn't unsafe. TypeScript go away.
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
     let dark = settings.state.dark;
     if (dark == null) dark = prefersDark;
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     settings.dispatch({
       type: 'setdark',
       dark: !dark,
@@ -28,9 +27,11 @@ const ThemeButton: FC = () => {
   }, [settings, prefersDark]);
 
   return (
-    <IconButton color="inherit" onClick={handleThemeIconClick}>
-      {dark ? <MoonIcon /> : <SunIcon />}
-    </IconButton>
+    <Tooltip title={messageFormatter('toggle_theme')}>
+      <IconButton color="inherit" onClick={handleThemeIconClick}>
+        {dark ? <MoonIcon /> : <SunIcon />}
+      </IconButton>
+    </Tooltip>
   );
 };
 
